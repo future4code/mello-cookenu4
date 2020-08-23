379844dd05458dd23468d30201b5ea7d0ef6694c
Jonatan Machado <jonatanmachado35@gmail.com>
2020-08-23 13:18:07 -0300

    feat: listUserbyId

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
-import UserController from '../controller/UserController';
+import CreateUserController from '../controller/CreateUserController';
+import ListUserController from '../controller/ListUserController';
 
 const userRouter = Router();
-const createUser = new UserController();
+const createUser = new CreateUserController();
+const listUser = new ListUserController();
 
 userRouter.post('/', createUser.create);
+userRouter.get('/:id', listUser.getUser);
 
 export default userRouter;
--- src/utils/hashManager.ts
+++ src/utils/hashManager.ts
@@ -4,7 +4,7 @@ class HashManager {
   async hash(text: string): Promise<string> {
     const rounds = Number(process.env.BCRYPT_COST);
     const salt = await bcrypt.genSalt(rounds);
-    const result = await bcrypt.hash(text, salt);
+    const result = bcrypt.hash(text, salt);
     return result;
   }
 
